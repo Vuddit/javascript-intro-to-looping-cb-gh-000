64f5e87a74772f7ff50a188899d0f356308200e4
Rohit Kale <ruk22@cam.ac.uk>
2020-04-30 22:48:12 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,17 @@
+function forLoop(arr) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      arr.push(`I am 1 strange loop.`);
+    }
+    else {
+      arr.push(`I am ${i} strange loops.`);
+    }
+  }
+  return arr
+}
+
+function whileLoop (n) {
+  while (n>0) {
+    console.log(--n)
+  }
+}
